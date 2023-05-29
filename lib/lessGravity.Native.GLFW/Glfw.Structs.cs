3415f0fd95b4ff343e02f61b56aacfd47da0c5ad
lessGravity <less.gravity@outlook.com>
2023-05-29 14:12:55 +0200

    Add libs

---
+++ lib/lessGravity.Native.GLFW/Glfw.Structs.cs
@@ -0,0 +1,19 @@
+﻿namespace lessGravity.Native.GLFW;
+
+public static unsafe partial class Glfw
+{
+    public struct VideoMode
+    {
+        public int Width;
+
+        public int Height;
+
+        public int RedBits;
+
+        public int GreenBits;
+
+        public int BlueBits;
+
+        public int RefreshRate;
+    }
+}
